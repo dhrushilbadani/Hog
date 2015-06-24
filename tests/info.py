79ba1b9bacbf79ee258f2142d5b736448d4cc60f
dhrushilbadani <badanidhrushil@berkeley.edu>
2015-06-24 22:38:34 +0530

    Twitter-Trends

---
+++ tests/info.py
@@ -0,0 +1,16 @@
+info = {
+  'hash_key': 'x2ge71fu26r3y5hh53c37ihbf7czs18r4iyyzvytd4gg41soesvcvcbxvc5lhxeemmghqsoiuav5jl94r3bsvudp5i1miqetv0z6dozy6e7nujmbs0f96rfqqy8fokj5',
+  'name': 'proj1',
+  'params': {
+    'doctest': {
+      'cache': """
+      import hog
+      from hog import *
+      """
+    }
+  },
+  'src_files': [
+    'hog.py'
+  ],
+  'version': '1.0'
+}
